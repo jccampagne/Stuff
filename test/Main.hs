ac692b8a4abd2d7d453987e40654dfdd4519e617
Jc <jc.campagne@gmail.com>
2017-02-14 23:11:23 +0000

    move quaternion tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,31 +1,10 @@
 {-# Language ScopedTypeVariables #-}
 
 import Test.HUnit
-import Quaternion
-
-qTest =
-    let v :: Q Double = Q 1 2 3 4 in
-    let r = negate $ negate $ Q 1 2 3 4 in
-    TestCase $ assertEqual "same" v r
-
-qNormTest0 =
-    let Q v 0 0 0 :: Q Double = abs $ Q 1 2 3 4 in
-    let r = sqrt 30 in
-    TestCase $ assertEqual "same" v r
-
-qNormTest1 =
-    let q = Q 1 2 3 4 in
-    let v = norm q in
-    let vv = v * v in
-    let r = 1*1 + 2*2 + 3*3 + 4*4 in
-    TestCase $ assertEqual "same" vv r
+import Test.TestQuaternion
 
 main :: IO ()
 main = do
-    counts <- runTestTT $
-        TestList [ TestLabel "simple" qTest
-                 , TestLabel "qnorm" qNormTest0
-                 , TestLabel "qnorm" qNormTest1
-                 ]
+    counts <- runTestTT Test.TestQuaternion.tests
     putStrLn $ show counts
     return ()
--- test/Test/TestQuaternion.hs
+++ test/Test/TestQuaternion.hs
@@ -1,8 +1,30 @@
-module Test.TestQuaternion where
+module Test.TestQuaternion (tests) where
+
+{-# Language ScopedTypeVariables #-}
 
 import Test.HUnit
-test1 = TestCase (assertEqual "for (foo 3)," (1,2) (foo 3))
+import Quaternion
 
+qTest =
+    let v = Q 1 2 3 4 in
+    let r = negate $ negate $ Q 1 2 3 4 in
+    TestCase $ assertEqual "same" v r
 
+qNormTest0 =
+    let Q v 0 0 0 = abs $ Q 1 2 3 4 in
+    let r = sqrt 30 in
+    TestCase $ assertEqual "same" v r
 
-tests = return []
+qNormTest1 =
+    let q = Q 1 2 3 4 in
+    let v = norm q in
+    let vv = v * v in
+    let r = 1*1 + 2*2 + 3*3 + 4*4 in
+    TestCase $ assertEqual "same" vv r
+
+tests =
+    TestList [ TestLabel "simple" qTest
+             , TestLabel "qnorm" qNormTest0
+             , TestLabel "qnorm" qNormTest1
+             ]
+
